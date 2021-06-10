048d166f69d32e6f364a606aa8432eb48ef7c13c
Erik <erikkayser843@gmail.com>
2021-06-10 14:07:31 -0300

    Exercícios aula05

---
+++ aula05/exarray.js
@@ -0,0 +1,51 @@
+let compras=['pão', 'café', 'feijão', 'batatas', 'arroz']
+console.log(compras)
+
+//o push mostra a posição do elemento que você chamou dentro da array
+console.log('o metodo push mostra a posição do elemento que você chamou dentro da array')
+console.log(compras.push('arroz'))
+
+//o pop exclui o ultimo item do array e o imprime na tela
+console.log('o metodo pop exclui o ultimo item do array e o imprime na tela')
+let ultimoItem= compras.pop()
+console.log(compras)
+console.log(ultimoItem)
+
+//o shift exclui o primeiro item do array e o imprime na tela
+console.log('o metodo shift exclui o primeiro item do array e o imprime na tela')
+
+let primeiroItem=compras.shift()
+console.log(compras)
+console.log(primeiroItem)
+
+//o unshift adiciona itens no inicio do array
+console.log('o metodo unshift adiciona itens no inicio do array')
+console.log(compras)
+compras.unshift('abacate')
+console.log(compras)
+compras.unshift('leite','manteiga')
+console.log(compras)
+
+// o .join retorna como string todos os elementos do array, e com um separador opcional
+console.log('o metodo join retorna como string todos os elementos do array, e com um separador opcional')
+let separadosPorPonto=compras.join('. ')
+let separadosPorLine=compras.join('_')
+console.log(separadosPorLine)
+console.log(separadosPorPonto)
+
+//o index of retorna a posição do item de tras pra frente
+//o indexOf retorna a posição do item da frente pra tras
+// se não for encontrado o item é retornado -1
+console.log('o metodo index of retorna a posição do item de tras pra frente')
+console.log(compras.indexOf('feijão'))
+
+console.log('o metodo indexOf retorna a posição do item da frente pra tras')
+console.log(compras.lastIndexOf('batatas'))
+
+console.log('mas se não for encontrado o item é retornado -1')
+console.log(compras.lastIndexOf('caju'))
+
+//o include retorna um valor booleano para o item pesquisado
+console.log('o metodo include retorna um valor booleano para o item pesquisado')
+console.log(compras.includes('leite'))
+console.log(compras.includes('miojo'))
