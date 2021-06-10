7e265b798ca84f100c7519bf7331ef9dddcb95a1
Erik <erikkayser843@gmail.com>
2021-06-10 15:54:10 -0300

    Exercícios aula05 edit pós aula

--- aula05/exarray.js
+++ aula05/exarray.js
@@ -9,14 +9,23 @@ console.log(compras.push('arroz'))
 console.log('o metodo pop exclui o ultimo item do array e o imprime na tela')
 let ultimoItem= compras.pop()
 console.log(compras)
+compras.pop()
 console.log(ultimoItem)
+console.log(compras)
 
 //o shift exclui o primeiro item do array e o imprime na tela
 console.log('o metodo shift exclui o primeiro item do array e o imprime na tela')
 
-let primeiroItem=compras.shift()
+let comprasShift=compras.shift()
+compras.shift()
 console.log(compras)
-console.log(primeiroItem)
+console.log(comprasShift)
+
+//let primeiroItem=compras.shift()
+console.log(compras)
+//console.log(primeiroItem)
+compras.shift()
+console.log(compras)
 
 //o unshift adiciona itens no inicio do array
 console.log('o metodo unshift adiciona itens no inicio do array')
@@ -26,15 +35,15 @@ console.log(compras)
 compras.unshift('leite','manteiga')
 console.log(compras)
 
-// o .join retorna como string todos os elementos do array, e com um separador opcional
+// o .join converte todos os elementos do array em string, e com um separador opcional
 console.log('o metodo join retorna como string todos os elementos do array, e com um separador opcional')
 let separadosPorPonto=compras.join('. ')
 let separadosPorLine=compras.join('_')
 console.log(separadosPorLine)
 console.log(separadosPorPonto)
 
-//o index of retorna a posição do item de tras pra frente
-//o indexOf retorna a posição do item da frente pra tras
+//o indexOf retorna a posição do item de tras pra frente
+//o lastIndexOf retorna a posição do item da frente pra tras
 // se não for encontrado o item é retornado -1
 console.log('o metodo index of retorna a posição do item de tras pra frente')
 console.log(compras.indexOf('feijão'))
