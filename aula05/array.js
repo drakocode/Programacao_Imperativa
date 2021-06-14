e07c1a7426572a56405098c233371ba8db154283
Erik Kayser <81787117+resyakKire@users.noreply.github.com>
2021-06-14 18:07:52 -0300

    Add files via upload

---
+++ aula05/array.js
@@ -0,0 +1,21 @@
+let filmes = ["star wars", "clube da luta",  "o poderoso chefão", "top gun",  "interestelar"]
+let cartoons = ["toy story", "Procurando Nemo", "kung-fu panda", "wally", "fortnite"]
+
+console.log(cartoons)
+console.log(filmes)
+
+function addItem(fuck){
+    fuck=true
+    // console.log(addItem(filmes))
+}
+
+var adicionar = filmes.push('Laranja');
+    // filmes.unshift("toy story")
+    // filmes.unshift("Procurando Nemo")
+    // filmes.unshift("kung-fu panda")
+    // filmes.unshift("kung-fu panda")
+    // filmes.unshift("wally")
+    // filmes.unshift("fortnite")
+addItem 
+// console.log('foi?',filmes)
+console.log('foi?',addItem('filmes'))
